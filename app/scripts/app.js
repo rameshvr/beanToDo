3bfc4b8ed19c52e537be76b8b85b139fa62f87c3
Ramesh <ramrajagopal@deloitte.com>
2015-09-30 15:55:09 +0530

    scaffolding the app

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name beanToDoApp
+ * @description
+ * # beanToDoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('beanToDoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name beanToDoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the beanToDoApp
+ */
+angular.module('beanToDoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
