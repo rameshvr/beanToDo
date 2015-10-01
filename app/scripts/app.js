55371ae04dc22fd5937af013954c342a98d2b7a6
Ramesh <ramrajagopal@deloitte.com>
2015-10-01 23:38:39 +0530

    adding local storage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,19 +15,18 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+     'LocalStorageModule'
   ])
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,22 +8,20 @@
  * Controller of the beanToDoApp
  */
 angular.module('beanToDoApp')
-    .controller('MainCtrl', function($scope) {
+    .controller('MainCtrl', function($scope, $timeout, localStorageService) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
 
-        $scope.todos = [{
-            text: 'ToDo1',
-            done: false,
-            priority:2
-        }, {
-            text: 'ToDo2',
-            done: true,
-            priority:-1
-        }];
+        var todosInStore = localStorageService.get('todos');
+        $scope.todos = todosInStore || [];
+
+        //keeping the objects in sync
+        $scope.$watch('todos', function() {
+            localStorageService.set('todos', $scope.todos);
+        }, true);
 
         $scope.addTodo = function() {
             $scope.todos.push({
@@ -31,18 +29,58 @@ angular.module('beanToDoApp')
                 done: false
             });
             $scope.formTodoText = '';
-        	$scope.updateLocalStorage($scope.todos);
-        };
-        
-        $scope.toggleDone = function(){
-        	this.item.done = !this.item.done;
-        	if(this.item.done === true){
-        		$.each($scope.todos, function(key){
-        			console.log(key);
-        		});
-        	}
         };
 
-        $scope.updateLocalStorage = function(){};
+        $scope.removeTodo = function(index) {
+            $timeout(function() {
+                $scope.todos.splice(index, 1);
+            }, 100);
+            // $scope.todos.push({
+            //     text: this.item.text,
+            //     done: true
+            // });
+        };
+
+        // $scope.handleKey = function(e) {
+        //     switch (e.which) {
+        //         case 40:
+        //         	$(e.target).parent().parent().toggleClass('hover');
+        //             break;
+        //         case 38:
+        //             break;
+        //         default:
+
+        //     }
+
+        // };
+
+        // $scope.moveUp = function(index) {
+        //     // $scope.todos.splice(index, 1);
+        //     console.log(index);
+        // };
+
+        // $scope.moveDown = function(index) {
+        //     // $scope.todos.splice(index, 1);
+        //     console.log(index);
+        // };
+
+        // Array.prototype.move = function(old_index, new_index) {
+        //     while (old_index < 0) {
+        //         old_index += this.length;
+        //     }
+        //     while (new_index < 0) {
+        //         new_index += this.length;
+        //     }
+        //     if (new_index >= this.length) {
+        //         var k = new_index - this.length;
+        //         while ((k--) + 1) {
+        //             this.push(undefined);
+        //         }
+        //     }
+        //     this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+        //     return this; // for testing purposes
+        // };
+
+
 
     });
